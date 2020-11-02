89cc0119c577ba71dbde97ab3ddb496467b30bab
Wooyang Son <sonwy102@gmail.com>
2020-11-02 19:49:20 +0000

    add login

--- crud.py
+++ crud.py
@@ -16,19 +16,25 @@ def create_user(email, password):
 
     return user
 
+
 def get_users():
+    """Return all users."""
 
     return User.query.all()
 
+
 def get_user_by_id(user_id):
+    """Return a user by id."""
 
     return User.query.get(user_id)
+
 
 def get_user_by_email(email):
     """Return a user by email."""
 
-    return User.query.filter(User.email = email).first()
+    return User.query.filter(User.email == email).first()
     
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
@@ -39,13 +45,18 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+
 def get_movies():
-    
+    """Return all movies."""
+
     return Movie.query.all()
 
-def get_movie_by_id(movie_id):
 
+def get_movie_by_id(movie_id):
+    """Return movie by id."""
+    
     return Movie.query.get(movie_id)
+
 
 def create_rating(score, movie, user):
     """Create and return a new rating."""
--- server.py
+++ server.py
@@ -11,10 +11,12 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
-# Replace this with routes and view functions!
 @app.route('/')
 def index():
+    """Show homepage."""
+
     return render_template('homepage.html')
+
 
 @app.route('/users', methods=['POST'])
 def register_user():
@@ -31,30 +33,61 @@ def register_user():
 
     return redirect('/')
 
+
+@app.route('/login')
+def login_user():
+    """Log user into their account."""
+
+    email = request.args.get('email')
+    password = request.args.get('password')
+    user = crud.get_user_by_email(email)
+
+    if user != None:
+        if user.password == password:
+            session['user'] = user.user_id
+            flash('Logged in successfully!')
+        else:
+            flash('Incorrect email/password.')
+    else:
+        flash('Account does not exist.')
     
+    return redirect('/')
+        
 
 @app.route('/movies')
 def show_all_movies():
+    """Show all movies in database"""
+    
     movies = crud.get_movies()
 
     return render_template('all_movies.html', movies=movies)
 
+
 @app.route('/movies/<movie_id>')
 def show_movie(movie_id):
+    """Show movie's details."""
 
     movie = crud.get_movie_by_id(movie_id)
+
     return render_template('movie_details.html', movie=movie)
+
 
 @app.route('/users')
 def show_all_users():
+    """Show all users."""
+
     users = crud.get_users()
+
     return render_template('all_users.html', users=users)
+
 
 @app.route('/users/<user_id>')
 def show_user(user_id):
+    """Show user info."""
 
     user = crud.get_user_by_id(user_id)
     return render_template('users_details.html', user=user)
+
 
 if __name__ == '__main__':
     connect_to_db(app)
